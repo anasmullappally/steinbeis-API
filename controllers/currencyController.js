b7137d3c66247bd052628d89b5584c07dca0b4b7
anasmullappally <mohammedanas362@gmail.com>
2022-10-27 12:08:38 +0530

    currency rate finding compleated

--- controllers/currencyController.js
+++ controllers/currencyController.js
@@ -1,5 +1,15 @@
+const axios = require("axios");
 const currencyRateFinding = async (req, res) => {
-  console.log(req);
+  try {
+    const currencyCode = req.query.currencyCode;
+    const rates = await axios.get("https://open.er-api.com/v6/latest/INR");
+    const rate = rates.data.rates[currencyCode];
+    rate
+      ? res.status(200).json({ rate, currencyCode })
+      : res.status(400).json({ message: "Invalid currency code" });
+  } catch (error) {
+    res.status(error.response.status).json({ error: error.message });
+  }
 };
 
 module.exports = { currencyRateFinding };
